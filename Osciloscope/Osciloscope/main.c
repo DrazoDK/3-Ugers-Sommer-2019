f39e924c18c5220cdec0061150d8ffa339267ada
David Kortholm <davidkortholm@gmail.com>
2019-06-06 13:39:14 +0200

    Merge

--- Osciloscope/Osciloscope/main.c
+++ Osciloscope/Osciloscope/main.c
@@ -1,16 +1,8 @@
-/*
- * Osciloscope.c
- *
- * Created: 06-Jun-19 13:26:27
- * Author : David
- */ 
-
 #include <avr/io.h>
 
 
 int main(void)
 {
-    /* Replace with your application code */
     while (1) 
     {
     }
